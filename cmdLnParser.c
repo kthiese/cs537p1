a9cdc579e8450dda5d7caf7322b4378cb8e9e26f
Kathryn Thiese <thiese@rockhopper-03.cs.wisc.edu>
2018-09-20 22:42:38 -0500

    Updated the command parser, created struct for linked list and processes, and traversing through proc file

--- cmdLnParser.c
+++ cmdLnParser.c
@@ -1,10 +1,30 @@
-
-
+#include <sys/types.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-int pid = -1;
+// Struct holding info for a process
+struct Process{
+	char * pid;
+	char *state;
+	int utime;
+	int stime;
+	int memory;
+	char *cmdline;
+};
+
+// Struct used for a linked list of processes.
+struct linklist {
+	struct Process *p;
+	struct linklist *next;
+};
+
+
+char *pid = NULL;
+int uid = 0;
 int s = 0;
 int U = 1;
 int S = 0;
@@ -12,36 +32,115 @@ int v = 0;
 int c = 1;
 
 int main (int argc, char *argv[]){
-	
+	uid = getuid();	
 	int opt = 0;
-	while ((opt = getopt (argc, argv, "p:sUSvc")) != -1) {
+	// Parses the command line and does the appropriate action
+	while ((opt = getopt (argc, argv, "p:s::U::S::v::c::")) != -1) {
 		switch (opt)
 		{
 			case 'p':
-	        	 pid = atoi(optarg);;
+	        	 pid = optarg;
   			 break;
 	      		case 's':
-	        	 s = 1;
-	        	 break;
+	        	 if (*optarg == '-'){
+				 break;
+			 }else{ 
+			 	s = 1;
+	        	 	break;
+			 }
 	     	        case 'U':
-	        	 U = 0;
-	        	 break;
+	        	 if (*optarg == '-'){
+				 U = 0;
+				 break;
+			 } else {
+			 	 break;
+			 }
 	      		case 'S':
-			 S = 1;
-	        	 break;
+			 if (*optarg == '-'){
+				 break;
+			 } else {
+			 	S = 1;
+	        	 	break;
+			 }
 	     	 	case 'v':
-			 v = 1;
-			 break;
+			 if (*optarg == '-') {
+				 break;
+			 } else {
+			 	v = 1;
+			 	break;
+			 }
 	      		case 'c':
-			 c = 0;
+			 if (*optarg == '-'){
+				 c = 0;
+			 } else {
+			 	break;
+			 }
+			case '?':
+			 printf("WRONG INPUT");
 			 break;
+
 	      }
 	}
 
-	getProcesses(pid);
 
-	printf("%d %d %d %d %d", s, U, S, v, c);
+	printf("%s %d %d %d %d %d %d", pid, uid, s, U, S, v, c);
+	
+	getProcesses();
+
+	return 0;
+}
+
+int getProcesses(){
+	readDirectory();
+
+	printf("%s: ", pid);
+	if (s == 1){
+//		printf("%s ", state);
+	}
+	if (U == 1) {
+//		printf("utime=%d ",p1->utime);
+	}
+	if (S == 1) {
+//		printf("stime=%d ", p1->stime);
+	}
+	if (v == 1) {
+//		printf("vmemory=%d ", p1->memory);
+	}
+	if (c == 1) {
+//		printf("[%s]", *p1->cmdline);
+	}
+
+	return 0;
+
+}
+
+int readDirectory(){
+	DIR *dir;
+	struct dirent *dp;
+	char *filename;
+	dir=opendir("/proc/");
+	// Declare linked list that holds the processes
+	struct linklist head;
+	while ((dp=readdir(dir)) != NULL){
+		// Do the following if there was a p flag
+		if (pid != NULL){
+			// This only takes the files in the directory that are numbers.
+			if (isdigit(dp->d_name[0])){
+				if (strcmp(dp->d_name, pid) == 0){
+					struct Process p1 = {NULL, NULL, 0, 0, 0, NULL};
+					p1.pid = dp->d_name;
+					head.p = p1;
+				}
+			}	
+		} else {
+			if (isdigit(dp->d_name[0])){
+
+			}
+		}
 
 
+
+		
+	}	
 	return 0;
 }
